ea05cc1ef2729e827dd51ce1fe8ecefffc253404
omerbenarieh <omerrbenarieh@gmail.com>
2024-02-17 22:23:23 +0200

    arrangin code and folders

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import "bootstrap/dist/css/bootstrap.css";
-import LogIn from "./logIn.tsx";
-ReactDOM.createRoot(document.getElementById("root")!).render(
+import 'bootstrap/dist/css/bootstrap.css';
+import App from './components/App.tsx';
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <LogIn />
+    <App />
   </React.StrictMode>
 );
