7f1c938c3ecdc98a3ae4d99b4bf4d0b141495bbb
Dor Sonego <dorsonego123@gmail.com>
2024-02-18 16:34:59 +0200

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./login.css";
-=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
->>>>>>> 256714cc9e217120616fedbe7000c26d9a322089
+import './login.css';
 
 import 'bootstrap/dist/css/bootstrap.css';
-import App from './components/App.tsx';
+
+import LogIn from './logIn.tsx';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <LogIn />
   </React.StrictMode>
 );
