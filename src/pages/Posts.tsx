74ac731d20f1267ca3d909b66c07eb0f50bf0d8a
Dor Sonego <dorsonego123@gmail.com>
2024-03-06 20:13:14 +0200

    bla

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,16 +1,32 @@
-import Post from '../components/Post';
-import PostForm from '../components/PostForm';
+import { useEffect } from "react";
+import Post from "../components/Post";
+import PostForm from "../components/PostForm";
+import { PostType } from "../helpers/types";
+import { getPostsAPI } from "../api/auth";
+import { useTokens } from "../hooks/useTokens";
 
-export default function Posts() {
+export default function Posts({
+  posts,
+  setPosts,
+}: {
+  posts: PostType[];
+  setPosts: (posts: PostType[]) => void;
+}) {
+  const { accessToken } = useTokens();
+  useEffect(() => {
+    async function renderPosts() {
+      const posts = await getPostsAPI(accessToken);
+      setPosts(posts);
+    }
+    renderPosts();
+  }, [accessToken, setPosts]);
   return (
     <div>
-      <PostForm />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <PostForm posts={posts} setPosts={setPosts} />
+      <h1>Number or posts: {posts.length}</h1>
+      {posts.map((post) => (
+        <Post key={post._id} post={post} setPosts={setPosts} posts={posts} />
+      ))}
     </div>
   );
 }
