3e15a5a752fa52312e26fbd64e392fdd8b63bf36
DorSonego <dorsonego123@gmail.com>
2024-03-13 22:42:35 +0200

    update

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,17 +1,44 @@
 import { CommentType } from '../helpers/types';
+import { User } from '../helpers/types';
+import ImageShape from './ImageShape';
 export default function Comment({
   comment,
-  setComments,
+
+  user,
 }: {
   comment: CommentType;
-  setComments: (comments: CommentType[]) => void;
+
+  user: User;
 }) {
   return (
     <>
-      <p>{comment.content}</p>
-      <div className="d-flex justify-content-between">
-        <div className="d-flex flex-row align-items-center">
-          <p className="small mb-0 ">{comment.writer.name}</p>
+      <div className="row d-flex justify-content">
+        <div className="col-md-8 col-lg-6">
+          <center>
+            <div
+              className="card shadow-0 border"
+              style={{
+                backgroundColor: '#f0f2f5',
+                width: '29rem',
+              }}
+            >
+              <div className="card-body p-8">
+                <div className="card mb-4">
+                  <div className="d-flex flex-row align-items-center">
+                    <div style={{ fontSize: '15px' }}>
+                      {comment.writer.name}
+                      <p>
+                        <ImageShape user={user} />
+                      </p>
+                    </div>
+                    <div className="card-body">
+                      <p>{comment.content}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </center>
         </div>
       </div>
     </>
--- src/pages/CommentList.tsx
+++ src/pages/CommentList.tsx
@@ -1,27 +1,31 @@
 import { CommentType } from '../helpers/types';
 import Comment from '../components/Comment';
-import { Dropdown, DropdownItem, Modal } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 
 export default function CommentList({
   comments,
-  setComments,
+
   handleCommentsClose,
   showComments,
 }: {
   comments: CommentType[];
-  setComments: (comments: CommentType[]) => void;
+
   handleCommentsClose: () => void;
   showComments: boolean;
 }) {
   return (
-    <div>
-      {comments.map(comment => (
-        <Comment
-          key={comment._id}
-          comment={comment}
-          setComments={setComments}
-        />
-      ))}
-    </div>
+    <Modal show={showComments} onHide={handleCommentsClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Comments</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {comments.map(comment => (
+          <Comment key={comment._id} comment={comment} user={comment.writer} />
+        ))}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary">Close</Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
