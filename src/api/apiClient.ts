9aa345ddf9314e7bffbeb310b1bf77355942fc2c
omerbenarieh <omerrbenarieh@gmail.com>
2024-03-18 00:33:50 +0200

    bla

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,8 +1,16 @@
 import axios, { CanceledError } from 'axios';
 
+let url: string;
+
+if (process.env.NODE_ENV === 'development') {
+  url = 'http://localhost:3000';
+} else {
+  url = 'https://193.106.55.189';
+}
+
 export { CanceledError };
 const apiClient = axios.create({
-  baseURL: 'https://193.106.55.189',
+  baseURL: url,
 });
 
 export default apiClient;
